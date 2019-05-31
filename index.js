2cd16ec7fb3c7a424f29e60a013ff0a4c0a605b8
dat.tieu <tieu.dat102@gmail.com>
2019-05-31 19:45:58 +0700

    fix CORS

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ mongoose.set('useCreateIndex', true)
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // production error handler
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
