06002997bd3be8e29f0c57b82dd102622f33d307
dat.tieu <tieu.dat102@gmail.com>
2019-06-03 16:56:20 +0700

    validate username unique

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,22 @@
 var bcrypt = require('bcrypt');
 var User = require('../models/userModel');
 
-exports.register = function (req, res) {
+async function checkUsername(value) {
+    var result = await User.findOne({username: value}).exec();
+    console.log(result);
+    return result;
+}
+
+exports.register = async function (req, res) {
+    if (!req.body.username || !req.body.password || !req.body.full_name) {
+        return res.status(400).send({message: "Invalid parameter"});
+    }
+    
+    const count = await User.countDocuments({username: req.body.username});
+    if(count > 0){
+        return res.status(400).send({message: "Username already exist"});
+    }
+    
     var newUser = new User(req.body);
 
     if (req.body.password) {
@@ -18,6 +33,8 @@ exports.register = function (req, res) {
             return res.json(user);
         }
     });
+
+
 };
 
 exports.login = function (req, res) {
@@ -33,16 +50,16 @@ exports.login = function (req, res) {
         if (!user || !user.comparePassword(req.body.password)) {
             return res.status(401).json({message: 'Username or Password not correct!'});
         }
-		
-		data = user.toJsonData();
-		data.access_token = user.getAccessToken();
+
+        data = user.toJsonData();
+        data.access_token = user.getAccessToken();
 
         res.status(200).json({user: data});
     });
 };
 
 exports.auth = function (req, res, next) {
-    if(req.user == null){
+    if (req.user == null) {
         return res.status(401).json({"message": "Unauthorization !"});
     }
     next();
